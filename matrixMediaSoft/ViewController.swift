ce2838675dc4971518732b5a1debfb0079d4253f
ErlingFX <75131227+ErlingFX@users.noreply.github.com>
2021-03-11 15:26:14 +0400

    Initial Commit

---
+++ matrixMediaSoft/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  matrixMediaSoft
+//
+//  Created by Alexander Nazarov on 11.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
